4d7f0f5f84972eee491819bb732434a0d35c9eaa
Ernest Offiong <ernest.offiong@gmail.com>
2019-07-10 15:28:59 +0100

    feat: added deployment to infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,12 @@
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config();
+}
+const fs = require('fs');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const INFURAID = fs.readFileSync(".env").toString().trim();
+
+console.log(INFURAID)
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +15,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+     ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${INFURAID}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   }
 };
