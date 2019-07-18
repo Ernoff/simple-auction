9a9462a8d00fc9148053377d8d6c8cfaab78a894
Ernest Offiong <ernest.offiong@gmail.com>
2019-07-18 14:37:26 +0100

    feat: added mortal design pattern

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,26 +1,28 @@
 pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract Auction is Pausable {
+contract Auction is Ownable, Pausable {
   // Parameters of the auction. Times are either
   // absolute unix timestamps (seconds since 1970-01-01)
   // or time periods in seconds.
-  address payable public beneficiary;
-  uint public auctionEndTime;
-  uint public biddingTime;
+    address payable public beneficiary;
+    address public owner;
+    uint public auctionEndTime;
+    uint public biddingTime;
   // Current state of the auction.
-  address public highestBidder;
-  uint public highestBid;
+    address public highestBidder;
+    uint public highestBid;
 
   // Allowed withdrawals of previous bids
-  mapping(address => uint) pendingReturns;
+    mapping(address => uint) pendingReturns;
   // Set to true at the end, disallows any change
-  bool ended;
+    bool ended;
 
   // Events that will be fired on changes.
-  event HighestBidIncreased(address bidder, uint amount);
-  event AuctionEnded(address winner, uint amount);
+    event HighestBidIncreased(address bidder, uint amount);
+    event AuctionEnded(address winner, uint amount);
 
   // The following is a so-called natspec comment,
   // recognizable by the three slashes.
@@ -29,62 +31,63 @@ contract Auction is Pausable {
 
   /// Create a simple auction with `_biddingTime`
   /// seconds bidding time on behalf of the
-  constructor(uint _biddingTime, address payable _beneficiary) public {
-    beneficiary = _beneficiary;
-    auctionEndTime = getTime() + _biddingTime;
-    biddingTime = _biddingTime;
-  }
+    constructor(uint _biddingTime, address payable _beneficiary) public {
+        beneficiary = _beneficiary;
+        auctionEndTime = getTime() + _biddingTime;
+        biddingTime = _biddingTime;
+        owner = msg.sender;
+    }
 
-  function getTime() internal view returns (uint256) {
-    return block.timestamp;
-  }
+    function getTime() internal view returns (uint256) {
+        return block.timestamp;
+    }
   /// Bid on the auction with the value sent
   /// together with this transaction.
   /// The value will only be refunded if the
   /// auction is not won.
-  function bid() public whenNotPaused() payable {
+    function bid() public whenNotPaused() payable {
     // The keyword payable
     // is required for the function to
     // be able to receive Ether.
       // Revert the call if the bidding
       // period is over.
-    require(getTime() <= auctionEndTime, 'Time has elapsed');
+        require(getTime() <= auctionEndTime, 'Time has elapsed');
       // If the bid is not higher, send the money back.
-    require(msg.value > highestBid, 'Bid is too low');
-    if (highestBidder != address(0x00)) {
+        require(msg.value > highestBid, 'Bid is too low');
+        if (highestBidder != address(0x00)) {
       // Sending back the money by simply using
       // highestBidder.send(highestBid) is a security risk
       // because it can be prevented by the caller by e.g.
       // raising the call stack to 1023. It is always safer
       // to let the recipient withdraw their money themselves.
-    pendingReturns[highestBidder] += highestBid;
+        pendingReturns[highestBidder] += highestBid;
     }
-    auctionEndTime += biddingTime;
-    highestBidder = msg.sender;
-    highestBid = msg.value;
-    emit HighestBidIncreased(msg.sender, msg.value);
-  }
+        auctionEndTime += biddingTime;
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit HighestBidIncreased(msg.sender, msg.value);
+    }
   /// Withdraw a bid that was overbid.
-  function withdraw() public whenNotPaused() returns (bool)  {
-    uint amount = pendingReturns[msg.sender];
-    if (amount > 0) {
+    function withdraw() public whenNotPaused() returns (bool)  {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
       // It is important to set this to zero because the recipient
       // can call this function again as part of the receiving call
       // before `send` returns.
-      pendingReturns[msg.sender] = 0;
+            pendingReturns[msg.sender] = 0;
       // msg.sender.transfer(amount);
-      if (!msg.sender.send(amount)) {
+            if (!msg.sender.send(amount)) {
       //   // No need to call throw here, just reset the amount owing
-        pendingReturns[msg.sender] = amount;
-        return false;
-      }
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
 
   /// End the auction and send the highest bid
   /// to the beneficiary.
-  function auctionEnd() public whenNotPaused() {
+    function auctionEnd() public whenNotPaused() {
     // It is a good guideline to structure functions that interact
     // with other contracts (i.e. they call functions or send Ether)
     // into three phases:
@@ -92,13 +95,17 @@ contract Auction is Pausable {
     // 2. performing actions (potentially changing conditions)
 
     // 1. Conditions
-    require(getTime() >= auctionEndTime, 'Auction is still in progress');
-    require(!ended, "auction End has already being called");
+        require(getTime() >= auctionEndTime, 'Auction is still in progress');
+        require(!ended, "auction End has already being called");
     // 2. Effects
-    ended = true;
-    emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+        emit AuctionEnded(highestBidder, highestBid);
 
     //3. Interaction
-    beneficiary.transfer(highestBid);
-  }
+        beneficiary.transfer(highestBid);
+    }
+
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
